3336b9ead86d76bb515e186976a5b0b7de95b332
Adam Zimny <adamczyk97@op.pl>
2025-10-07 23:40:29 +0200

    Add backend project generated by Spring Initializr

---
+++ backend/src/main/java/az/project/VocabularyBuilder/VocabularyBuilderApplication.java
@@ -0,0 +1,13 @@
+package az.project.VocabularyBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyBuilderApplication.class, args);
+	}
+
+}
