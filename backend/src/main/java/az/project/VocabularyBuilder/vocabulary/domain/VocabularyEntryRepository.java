6d269dcc5468ff1cfd095e852ed75eb23cb14437
Adam Zimny <adamczyk97@op.pl>
2025-10-16 23:25:51 +0200

    Add new jpql queries for getting random words for quiz based on status or category it belongs

--- backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/VocabularyEntryRepository.java
+++ backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/VocabularyEntryRepository.java
@@ -1,11 +1,13 @@
 package az.project.VocabularyBuilder.vocabulary.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +18,19 @@ public interface VocabularyEntryRepository extends JpaRepository<VocabularyEntry
             @Param("word") String wordPhraseEn,
             @Param("pos") PartOfSpeech partOfSpeech
     );
+
+    @Query("SELECT e FROM VocabularyEntry e WHERE e.masteryStatus = :status " +
+            "AND (:#{#contextSources} IS NULL OR e.contextSource IN :contextSources) " +
+            "ORDER BY FUNCTION('RANDOM')") // Użycie natywnej funkcji RANDOM() dla PostgreSQL
+    List<VocabularyEntry> findRandomCandidates(@Param("status") MasteryStatus status,
+                                               @Param("contextSources") List<ContextSource> contextSources,
+                                               Pageable pageable);
+
+    // 2. Pobieranie pozostałych słówek (wypełnianie luki)
+    @Query("SELECT e FROM VocabularyEntry e WHERE e.id NOT IN :usedIds " +
+            "AND (:#{#contextSources} IS NULL OR e.contextSource IN :contextSources) " +
+            "ORDER BY FUNCTION('RANDOM')")
+    List<VocabularyEntry> findRandomRemaining(@Param("usedIds") List<Long> usedIds,
+                                              @Param("contextSources") List<ContextSource> contextSources,
+                                              Pageable pageable);
 }
