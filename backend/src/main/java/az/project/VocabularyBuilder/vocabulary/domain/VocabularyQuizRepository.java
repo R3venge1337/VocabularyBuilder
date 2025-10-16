2d54f8cae5023ec95d8b1bcf1b5220a222bead9d
Adam Zimny <adamczyk97@op.pl>
2025-10-16 23:27:06 +0200

    Add new jpql queries for getting recent quiz history,

--- backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/VocabularyQuizRepository.java
+++ backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/VocabularyQuizRepository.java
@@ -1,8 +1,30 @@
 package az.project.VocabularyBuilder.vocabulary.domain;
 
+import az.project.VocabularyBuilder.vocabulary.dto.QuizHistoryDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface VocabularyQuizRepository extends JpaRepository<VocabularyQuiz, Long> {
+    Optional<VocabularyQuiz> findByQuizUuid(final UUID uuid);
+
+    @Query(value = """
+                SELECT 
+                    q.quizUuid,
+                    q.scoreCorrect,
+                    q.scoreTotal,
+                    q.accuracyPercent,
+                    q.durationSeconds,
+                    q.dateCompleted
+                FROM VocabularyQuiz q
+                ORDER BY q.dateCompleted DESC
+                LIMIT :limit
+            """)
+    List<QuizHistoryDto> findRecentQuizzes(@Param("limit") int limit);
 }
