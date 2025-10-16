3d3de2f6c0a701b31279d288ae7a31f512d87131
Adam Zimny <adamczyk97@op.pl>
2025-10-16 23:28:00 +0200

    Add facade and implementation of methods set in facade

--- backend/src/main/java/az/project/VocabularyBuilder/vocabulary/VocabularyQuizFacade.java
+++ backend/src/main/java/az/project/VocabularyBuilder/vocabulary/VocabularyQuizFacade.java
@@ -1,4 +1,21 @@
 package az.project.VocabularyBuilder.vocabulary;
 
+import az.project.VocabularyBuilder.vocabulary.domain.ContextSource;
+import az.project.VocabularyBuilder.vocabulary.dto.QuizDto;
+import az.project.VocabularyBuilder.vocabulary.dto.QuizHistoryDto;
+import az.project.VocabularyBuilder.vocabulary.dto.QuizResultsDto;
+import az.project.VocabularyBuilder.vocabulary.dto.QuizViewDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface VocabularyQuizFacade {
+
+    QuizDto generateQuizWords(final int count, final List<ContextSource> contextSources);
+
+    QuizViewDto getQuizByUuid(final UUID quizUuid);
+
+    List<QuizHistoryDto> getRecentQuizzes(final int limit);
+
+    QuizViewDto saveQuizResults(final QuizResultsDto results);
 }
