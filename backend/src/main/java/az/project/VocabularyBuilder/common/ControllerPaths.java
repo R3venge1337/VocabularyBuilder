de7d9d0235c0ec5e098a980940c17971c7b69156
Adam Zimny <adamczyk97@op.pl>
2025-10-17 22:02:17 +0200

    Add error messages i route paths for controllers

--- backend/src/main/java/az/project/VocabularyBuilder/common/ControllerPaths.java
+++ backend/src/main/java/az/project/VocabularyBuilder/common/ControllerPaths.java
@@ -5,6 +5,7 @@ public class ControllerPaths {
     // Ścieżki Bazowe Kontrolerów
     public static final String ENTRY_BASE = "/vocabulary";
     public static final String QUIZ_BASE = " /quizzes";
+    public static final String TICTACTOE_BASE = " /games";
 
     // Szczegółowe Ścieżki Quizów
     public static final class Quiz {
@@ -23,5 +24,15 @@ public class ControllerPaths {
         }
 
         public static final String BY_ID = ENTRY_BASE + "/{id}";        // GET/PUT/DELETE /api/entries/{id}
+    }
+
+    public static final class TicTacToe {
+        private TicTacToe() {
+        }
+
+        public static final String GAME_START = TICTACTOE_BASE + "/start";
+        public static final String GAME_MOVE = TICTACTOE_BASE + "/move";
+        public static final String GAME_HISTORY = TICTACTOE_BASE + "/history";
+        public static final String GAME_REPLAY = TICTACTOE_BASE + "/replay/{sessionUuid}";
     }
 }
