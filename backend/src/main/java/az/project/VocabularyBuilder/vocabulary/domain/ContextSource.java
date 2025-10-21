61cac46a89320015c72250761c8b6326bae91fa2
Adam Zimny <adamczyk97@op.pl>
2025-10-21 13:39:09 +0200

    Add new contextSource types for enum

--- backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/ContextSource.java
+++ backend/src/main/java/az/project/VocabularyBuilder/vocabulary/domain/ContextSource.java
@@ -2,9 +2,11 @@ package az.project.VocabularyBuilder.vocabulary.domain;
 
 public enum ContextSource {
     MOVIE,
-    SERIES, // Dodajmy osobno, bo ma odcinki
+    SERIES,
     BOOK,
     DICTIONARY,
     WEBSITE,
+    COURSE,
+    CONVERSATION,
     GENERAL // Dla słówek, które nie mają tytułu/odcinka
 }
