0974c33c46a1889e2ac12a8adbd26a5d9242924d
Adam Zimny <adamczyk97@op.pl>
2025-10-21 23:51:32 +0200

    Add vocabulary create form to vocabulary page

--- frontend/src/app/features/vocabulary-quiz/pages/vocabulary-page/vocabulary-page.ts
+++ frontend/src/app/features/vocabulary-quiz/pages/vocabulary-page/vocabulary-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { VocabularyTableComponent } from '../../components/vocabulary-table-component/vocabulary-table-component';
+import { VocabularyAddFormComponent } from "../../components/vocabulary-add-form-component/vocabulary-add-form-component";
 
 @Component({
   selector: 'app-vocabulary-page',
-  imports: [VocabularyTableComponent],
+  imports: [VocabularyTableComponent, VocabularyAddFormComponent],
   templateUrl: './vocabulary-page.html',
   styleUrl: './vocabulary-page.scss'
 })
