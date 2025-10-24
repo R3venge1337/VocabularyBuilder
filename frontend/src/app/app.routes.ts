1589b75407470dfc049c61aeab18cc8b71eb3aba
Adam Zimny <adamczyk97@op.pl>
2025-10-24 02:29:32 +0200

    Add quiz path and load vocabulary-quiz-page by lazy loading

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,6 +9,10 @@ export const routes: Routes = [
         path: 'vocabulary', 
         loadComponent: () => import('./features/vocabulary-quiz/pages/vocabulary-page/vocabulary-page').then(m => m.VocabularyPage)
     },
+    {
+        path: 'quiz',
+        loadComponent: () => import('./features/vocabulary-quiz/pages/vocabluary-quiz-page/vocabluary-quiz-page').then(m => m.VocabluaryQuizPage)
+    },
     { 
         path: '', 
         redirectTo: 'home', 
